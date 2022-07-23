f62e0fbc1295ac7996baed31abaf939900a0f83b
Yusuke Sangenya <longinus.eva@gmail.com>
2022-07-23 20:30:08 +0900

    compile on intel

--- ext/build.rs
+++ ext/build.rs
@@ -29,6 +29,7 @@ fn main() {
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .blocklist_type("_bindgen_ty_1")
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
